0d694481d567d4744fc6661e96d849f5217d3bd4
zelihami <72530208+zelihami@users.noreply.github.com>
2021-07-11 19:22:45 +0300

    Add files via upload

---
+++ ödev 3.sql
@@ -0,0 +1,9 @@
+/*ÖDEV 3*/
+select country from country
+where country LIKE 'A%a'
+
+select country from country
+where country LIKE '_____%n'
+
+select title from film 
+where tıtle ILIKE '%t%t%t%t%'
